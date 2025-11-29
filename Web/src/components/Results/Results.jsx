f707321648df8a9a16d70529b59d144b7fb7b77d
axel <axlgarcia080498@gmail.com>
2025-11-29 16:34:02 -0300

    CSS results finish

--- Web/src/components/Results/Results.jsx
+++ Web/src/components/Results/Results.jsx
@@ -1,15 +1,25 @@
+import './Results.css'
+import preguntados from '../../assets/preguntados.png'
+
 const ResultsScreen = ({ correct, onRestart }) => {
      return (
-    <div>
-      <h1>Resultado final</h1>
-      <p>
-        Respuestas correctas: {correct} / 10  {/* Pongo 10 porque es el máximo por lo que miré */}
-      </p>
+      <>
+        <div className='containerResult' >
+          <h1>Resultado final</h1>
+          <p className='text'>
+            Respuestas correctas: {correct} / 10  {/* Pongo 10 porque es el máximo por lo que miré */}
+          </p>
 
-      <button onClick={onRestart}>
-        Jugar otra vez
-      </button>
-    </div>
+          <button onClick={onRestart} className='boton' >
+            Jugar otra vez
+          </button>
+        </div>
+
+        <div className='containerImg'>
+            <img src={preguntados} alt="Logo de preguntados" className='imgPreg'/>
+        </div>
+
+      </>
   );
 }
 
