d7d0042beb5c5e0c27ae0b1ab778fa4692ddd4e0
axel <axlgarcia080498@gmail.com>
2025-11-30 17:12:53 -0300

    responsive in Start.css

--- Web/src/services/questions.js
+++ Web/src/services/questions.js
@@ -1,7 +1,7 @@
 import api from './api'
 
 //SE obtienen las preguntas
-const getQuestions = async(difficulty) =>{
+const getQuestions = async(difficulty='easy') =>{
     const res = await api.get('/api/questions',{params: {difficulty}});
     return res.data;
 }
